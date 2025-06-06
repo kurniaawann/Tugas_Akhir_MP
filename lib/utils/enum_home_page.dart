5368f2b32d326a3741c793e29afc4fa5f2cb8dc8
kurniaawann <kurni.aawann19@gmail.com>
2025-06-06 16:36:32 +0700

    Add missing image assets for subjects in HomeImageAsset enum

--- lib/utils/enum_home_page.dart
+++ lib/utils/enum_home_page.dart
@@ -1,7 +1,11 @@
 enum HomeImageAsset {
   bahasaIndonesia('assets/images/Bahasa_Indonesia.jpg'),
   matematika('assets/images/Matematika.png'),
-  ipa('assets/images/Ipa.jpg');
+  ipa('assets/images/Ipa.jpg'),
+  ppkn('assets/images/ppkn.jpg'),
+  seniBudaya('assets/images/Seni-Budaya.jpg'),
+  pjok('assets/images/Pjok.jpg'),
+  bahasaInggris('assets/images/Bahasa_Inggris.jpg');
 
   final String path;
   const HomeImageAsset(this.path);
